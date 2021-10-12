db3785286b737eab4e6c1ffb1c5ddc2fa0b4808d
Derpius <49565664+Derpius@users.noreply.github.com>
2021-10-12 23:59:09 +0100

    Initial vscode extension implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nanos World support for sumneko's Lua language server extension
+
+Provides stubs of Nanos World's Lua API to the sumneko Lua language extension.  
+All stubs are generated automatically by dumping the client and server globals, and datatypes (metatables) are retreived from the registry.  
+
+## Requirements
+
+https://github.com/sumneko/lua-language-server
---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// Cleaned up version of https://github.com/sumneko/lua-language-server/wiki/EmmyLua-Libraries
+export function setExternalLibrary(extension: vscode.Extension<any>, folder: string, enable: boolean) {
+	const extensionPath = extension.extensionPath;
+	const folderPath = extensionPath + "\\" + folder;
+
+	const config = vscode.workspace.getConfiguration("Lua");
+	const library: string[] | undefined = config.get("workspace.library");
+
+	if (library && extensionPath) {
+		// remove any older versions of our path e.g. "publisher.name-0.0.1"
+		for (let i = library.length - 1; i >= 0; i--) {
+			const el = library[i];
+			const isSelfExtension = el.indexOf(extension.id) > -1;
+			const isCurrentVersion = el.indexOf(extensionPath) > -1;
+			if (isSelfExtension && !isCurrentVersion) {
+				library.splice(i, 1);
+			}
+		}
+
+		const index = library.indexOf(folderPath);
+		if (enable) {
+			if (index === -1) {
+				library.push(folderPath);
+			}
+		} else {
+			if (index > -1) {
+				library.splice(index, 1);
+			}
+		}
+
+		config.update("workspace.library", library, true);
+	}
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	setExternalLibrary(context.extension, "EmmyLua", true);
+	console.log("Nanos World sumneko bindings loaded!");
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
