5bec281d8b9f0f9d1e5d1237a7f25b78fde0ee17
Derpius <49565664+Derpius@users.noreply.github.com>
2022-08-08 21:43:50 +0100

    Octokit test

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,24 @@ import * as fs from "fs";
 console.log("Building documentation...");
 
 const TOKEN = getInput("github-token");
-const REPO_PATH = getInput("repository-path");
+const REPO_OWNER = getInput("repository-owner");
+const REPO_NAME = getInput("repository-name");
+const REPO_BRANCH = getInput("repository-branch");
 
-console.log(`Path: ${REPO_PATH}`);
+const octokit = getOctokit(TOKEN);
 
-console.log(`${context.serverUrl}/${context.repo.owner}/${context.repo.repo}`);
+async function buildDocs() {
+	const response = await octokit.request("GET /repos/{owner}/{repo}/git/trees/{tree_sha}", {
+		owner: REPO_OWNER,
+		repo: REPO_NAME,
+		tree_sha: REPO_BRANCH,
+		recursive: "1"
+	});
 
-fs.mkdirSync("./docs");
-fs.writeFileSync("./docs/annotations.lua", "---@meta\n");
+	console.log(response.data.tree);
 
-console.log("Build finished");
+	fs.mkdirSync("./docs");
+	fs.writeFileSync("./docs/annotations.lua", "---@meta\n");
+}
+
+buildDocs().then(() => console.log("Build finished"));
