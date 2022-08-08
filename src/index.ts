ec6d59f91d6a84be87864860e2f94a6321c0176c
Derpius <49565664+Derpius@users.noreply.github.com>
2022-08-08 20:55:00 +0100

    Add deploy step

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { getInput } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
+import * as fs from "fs";
 
 console.log("Building documentation...");
 
@@ -9,5 +10,8 @@ const REPO_PATH = getInput("repository-path");
 console.log(`Path: ${REPO_PATH}`);
 
 console.log(`${context.serverUrl}/${context.repo.owner}/${context.repo.repo}`);
+
+fs.mkdirSync("./docs");
+fs.writeFileSync("./docs/annotations.lua", "---@meta\n");
 
 console.log("Build finished");
