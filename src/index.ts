e823aa0881f0396335a900f3e270e193e6e41be5
Derpius <49565664+Derpius@users.noreply.github.com>
2022-08-08 22:11:18 +0100

    Implement WIP docgen from master

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { getInput } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
+
 import * as fs from "fs";
+import * as path from "path";
+
+import { Authority, DocClass } from "./schema";
 
 console.log("Building documentation...");
 
@@ -11,7 +15,72 @@ const REPO_BRANCH = getInput("repository-branch");
 
 const octokit = getOctokit(TOKEN);
 
+function generateAuthorityString(authority: Authority) {
+	switch (authority) {
+		case Authority.Server:
+			return "<img src=\"https://github.com/Derpius/nanosworld-vscode/blob/master/assets/server-only.png?raw=true\" height=\"10\"> `Server Side`";
+		case Authority.Client:
+			return "<img src=\"https://github.com/Derpius/nanosworld-vscode/blob/master/assets/client-only.png?raw=true\" height=\"10\"> `Client Side`";
+		case Authority.Authority:
+			return "<img src=\"https://github.com/Derpius/nanosworld-vscode/blob/master/assets/authority-only.png?raw=true\" height=\"10\"> `Authority Side`";
+		case Authority.Both:
+			return "<img src=\"https://github.com/Derpius/nanosworld-vscode/blob/master/assets/both.png?raw=true\" height=\"10\"> `Client/Server Side`";
+		default:
+			return `Invalid authority "${authority}" in documentation`;
+	}
+}
+
+function generateClassAnnotations(cls: DocClass): string {
+	let inheritance = "";
+	if (cls.inheritance !== undefined) {
+		inheritance = ` : ${cls.inheritance[0]}`;
+		for (let i = 1; i < cls.inheritance.length; i++) {
+			inheritance += `, ${cls.inheritance[i]}`;
+		}
+	}
+
+	let constructor = "";
+	if (cls.constructor !== undefined) {
+		let params = "";
+		let paramNames = "";
+
+		cls.constructor.forEach(function (param) {
+			params += `\n---@param ${param.name} ${param.type} ${param.description ? param.description + " " : ""}`;
+			if (param.default !== undefined) params += `(Default: ${param.default})`;
+
+			paramNames += param.name + ", ";
+		});
+
+		constructor = `
+---${generateAuthorityString(cls.authority)}
+---
+---${cls.description}${params}
+---@return ${cls.name}
+local function ${cls.name}(${paramNames.slice(0, -2)}) end
+`;
+	}
+
+	let staticFunctions = "";
+
+	let functions = "";
+
+	let events = "";
+
+	return `
+---${generateAuthorityString(cls.authority)}
+---
+---${cls.description.replaceAll("\n", "\n---\n---")}
+---@class ${cls.name}${inheritance}
+local ${cls.name}_meta = {}
+${constructor}
+${staticFunctions}
+${functions}
+${events}`;
+}
+
 async function buildDocs() {
+	let output = "---@meta\n";
+
 	const response = await octokit.request("GET /repos/{owner}/{repo}/git/trees/{tree_sha}", {
 		owner: REPO_OWNER,
 		repo: REPO_NAME,
@@ -19,10 +88,31 @@ async function buildDocs() {
 		recursive: "1"
 	});
 
-	console.log(response.data.tree);
+	response.data.tree.filter(function (entry) {
+		return entry.type === "blob" && entry.path?.endsWith(".json");
+	}).forEach(async function (entry) {
+		if (entry.path === undefined) return;
+	
+		const response = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+			accept: "application/vnd.github+json",
+			owner: REPO_OWNER,
+			repo: REPO_NAME,
+			path: entry.path,
+			ref: REPO_BRANCH
+		});
 
-	fs.mkdirSync("./docs");
-	fs.writeFileSync("./docs/annotations.lua", "---@meta\n");
+		// Process file
+		const file = response.data;
+		if (file.content === undefined) return;
+
+		const fileContents = JSON.parse(atob(file.content.replaceAll("\n", "")));
+
+		// Write annotations
+		output += generateClassAnnotations(fileContents);
+	});
+
+	await fs.promises.mkdir("./docs");
+	await fs.promises.writeFile("./docs/annotations.lua", output);
 }
 
 buildDocs().then(() => console.log("Build finished"));
